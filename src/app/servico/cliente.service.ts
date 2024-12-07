e3a935f68828884e94cd6769179e89155a6cdd4a
Edcristo <edcristogoncalves@gmail.com>
2024-12-07 09:08:50 -0300

    Configurando serviços

---
+++ src/app/servico/cliente.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClienteService {
+
+  // Url da API
+  private url:string = 'http://localhost:8080';
+
+  // Construtor
+  constructor(private http:HttpClient) { }
+}
