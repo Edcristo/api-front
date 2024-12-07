cd5d5509811f89560055e7cdc323c1e22a5e7297
Edcristo <edcristogoncalves@gmail.com>
2024-12-07 09:26:42 -0300

    Listagem de clientes

--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Cliente } from '../modelo/Cliente';
+import { ClienteService } from '../servico/cliente.service';
 
 @Component({
   selector: 'app-principal',
@@ -6,6 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./principal.component.css']
 })
 export class PrincipalComponent {
-  //Variável para visibilidade dos botões
-  btnCadastro:boolean = true;
+  // Variável para visibilidade dos botões
+  btnCadastro: boolean = true;
+
+  // JSON de clientes
+  clientes: Cliente[] = [];
+
+  // Construtor
+
+  constructor(private servico: ClienteService) {
+
+  }
+
+  // Método de seleção
+  selecionar(): void {
+    this.servico.selecionar().subscribe(retorno => this.clientes = retorno);
+  }
 }
--- src/app/servico/cliente.service.ts
+++ src/app/servico/cliente.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Cliente } from '../modelo/Cliente';
 
 @Injectable({
   providedIn: 'root'
@@ -7,8 +9,15 @@ import { Injectable } from '@angular/core';
 export class ClienteService {
 
   // Url da API
-  private url:string = 'http://localhost:8080';
+  private url: string = 'http://localhost:8080';
 
   // Construtor
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) {
+    // Método para selecionar todos os clientes
+  }
+  selecionar(): Observable<Cliente[]> {
+    return this.http.get<Cliente[]>(this.url);
+  }
 }
+
+
